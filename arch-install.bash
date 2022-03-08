c4ca8e76ef79077ce89035e67344f05ceead6344
Le Thien Cuong <cuong.lthien@gmail.com>
2022-03-08 10:29:26 +0700

    Update arch-install.bash

--- arch-install.bash
+++ arch-install.bash
@@ -2,10 +2,10 @@
 
 ESSENTIAL_PACKAGES="sudo polkit vim ranger git base-devel man-db man-pages texinfo tldr neofetch wget zip"
 
-echo "Platform: 1. WSL, 2. Virtual nachine, 3. Real machine"
+echo "Platform: 1. WSL, 2. Virtual machine, 3. Real machine"
 read -p "Which platform to install? "
 case $REPLY in
     1) source ./arch-install-wsl.bash;;
-    2) source ./arch-install-vmware.bash;;
+    2) source ./arch-install-vm.bash;;
 esac
 
