e11f235cd10d7d13bde701c39f9a3e83d62998ee
lewwcom <lewwcom@outlook.com>
2022-10-12 21:22:44 +0700

    Add bc

--- arch-install.bash
+++ arch-install.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ESSENTIAL_PACKAGES="sudo polkit vim ranger git base-devel man-db man-pages texinfo tldr neofetch wget zip unzip starship bash-completion tree bat httpie htop dictd glow difftastic go-yq mcfly"
+ESSENTIAL_PACKAGES="sudo polkit vim ranger git base-devel man-db man-pages texinfo tldr neofetch wget zip unzip starship bash-completion tree bat httpie htop dictd glow difftastic go-yq mcfly bc"
 
 echo "Platform: 1. WSL, 2. Virtual machine (VMWare - UEFI)"
 read -p "Which platform to install? "
