21d6b722645caa6770ce6a1df2d449c637e91c40
lewwcom <lewwcom@outlook.com>
2022-11-04 09:24:59 +0700

    Starship config

--- personalize-bash.bash
+++ personalize-bash.bash
@@ -4,19 +4,19 @@
 # sh -c "$(curl -fsSL https://starship.rs/install.sh)" -> Install using package manager
 mkdir -p ~/.config
 starship preset nerd-font-symbols > ~/.config/starship.toml
-echo '
-[username]
-show_always = true
-format = "[$user]($style)@(bold dimmed)"
-
-[hostname]
-ssh_only = false
-format = "[$hostname $ssh_symbol]($style)in "
-style = "bold yellow"
-
-[time]
-disabled = false
-time_format = "%R"' >> ~/.config/starship.toml
+# echo '
+# [username]
+# show_always = true
+# format = "[$user]($style)@(bold dimmed)"
+#
+# [hostname]
+# ssh_only = false
+# format = "[$hostname $ssh_symbol]($style)in "
+# style = "bold yellow"
+#
+# [time]
+# disabled = false
+# time_format = "%R"' >> ~/.config/starship.toml
 
 # install lightline - vim
 git clone https://github.com/itchyny/lightline.vim ~/.vim/pack/plugins/start/lightline
